f79a7c8a86a44f6a89f2502e4ad51141a5ea0f00
Ivan Poliarush <sashapolarush@gmail.com>
2024-10-09 18:24:26 +0300

    feat: update user model

--- src/modules/user/models/user.model.ts
+++ src/modules/user/models/user.model.ts
@@ -4,6 +4,9 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 export class User {
   @Prop({ unique: true })
   email: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
